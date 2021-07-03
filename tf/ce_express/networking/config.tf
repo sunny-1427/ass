382108caab6c85824bb18359a6dd86f9cb73fab2
surya <suryakirank6@gmail.com>
2021-07-03 01:27:19 +0000

    Edited config.tf

--- tf/ce_express/networking/config.tf
+++ tf/ce_express/networking/config.tf
@@ -4,12 +4,6 @@ terraform {
     path = "terraform.tfstate"
   }
 }
-
-provider “aws” {
- region = “us-west-2”
- shared_credentials_file = “/Users/tf_user/.aws/creds”
- profile = “customprofile”
-}
 [config]
 region=us-west-2
 output=json
